32974a715fef591bf73c984be421bb6f0fac3605
Nigostarr <wlckdgus5833@naver.com>
2020-07-13 22:42:45 +0900

    #1. Build essential function for shooting.

--- visionKitPractice/ViewController.swift
+++ visionKitPractice/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import VisionKit
 
 class ViewController: UIViewController {
 
@@ -15,6 +16,28 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func touchUpInsideCameraButton(_ sender: Any) {
+        configureDocumentView()
+    }
+    
+    private func configureDocumentView() {
+        let scanningDocumentVC = VNDocumentCameraViewController()
+        scanningDocumentVC.delegate = self
+        self.present(scanningDocumentVC, animated: true, completion: nil)
+    }
 }
 
+extension ViewController:VNDocumentCameraViewControllerDelegate {
+    
+    func documentCameraViewController(_ controller: VNDocumentCameraViewController, didFinishWith scan: VNDocumentCameraScan) {
+        
+        for pageNumber in 0..<scan.pageCount {
+            
+            let image = scan.imageOfPage(at: pageNumber)
+            
+            print(image)
+        }
+        
+        controller.dismiss(animated: true, completion: nil)
+    }
+}
